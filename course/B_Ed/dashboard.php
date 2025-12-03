d46f02a98b68bb12257f160635c5cfa457a20def
Prem Kumar <premkr017@gmail.com>
2025-12-04 02:51:47 +0530

    Update B.Ed admission page and dashboard; correct redirection and update title for improved clarity.

--- course/B_Ed/dashboard.php
+++ course/B_Ed/dashboard.php
@@ -21,7 +21,7 @@
     <main class="container mx-auto py-12 px-4">
 
         <h1 class="text-3xl font-bold text-center text-blue-700 mb-10">
-            User Dashboard
+            Welcome B.Ed Dashboard
         </h1>
 
         <!-- PROFILE CARD -->
